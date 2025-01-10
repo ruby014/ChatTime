f5efb1edcce83e208fa7e680cfe8f5dc8d880bac
Ruchelle <rbaybayan2@myseneca.ca>
2025-01-10 18:15:30 -0500

    backend to handle when user joins, leaves, profile pic generation

--- app.py
+++ app.py
@@ -5,9 +5,41 @@ import random
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# python dict. Store connected users. Key is socketid, value is username and avatarUrl
+users = {}
+
 @app.route('/')
 def index():
         return render_template('index.html')
+
+@socketio.on('connect') # listening for connect event 
+def handle_connect():
+        username = f"User_{random.randInt(10000,9999)}"
+        gender = random.choice(["girl","boy"])
+        # as per this randomly generated url, profile pic will be different
+        avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"
+
+        users[request.sid] = { "username":username,"avatar":avatar_url}
+
+        emit("user_joined", {"username":username,"avatar":avatar_url},broadcast=True)
+
+        emit("set_username", {"username":username})
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user = users.pop(request.sid, None)
+    if user:
+        emit("user_left",{"username":user["username"]},broadcast=True)
+
+@socketio.on('send_message')
+def handle_message(data):
+    user = users.get(request.sid)
+    if user:
+        emit("new_message", {
+              "username":user["username"], 
+              "avatar":user["avatar"],
+              "message":data["message"]
+        }, broadcast=True)
 
 if __name__ == "__main__":
         socketio.run(app); 
