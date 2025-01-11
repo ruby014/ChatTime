3262706a39fc33808d419be5841a7e33722f2da3
Ruchelle <rbaybayan2@myseneca.ca>
2025-01-10 19:30:03 -0500

    implemented send message and update username functiionality

--- templates/script.js
+++ templates/script.js
@@ -25,6 +25,37 @@ const socket = io(); // get socket instance to enable handling of events to back
             addMessage(data.message,"user", data.username, data.avatar)
         })
 
-        socket.on("username_updated", data => {
-            addMessage(data.message,"user", data.username, data.avatar)
+        socket.on("username_updated", (data) => {
+            addMessage(`${data.old_username} changed their name to ${data.new_username}`, "system")
+
+            if(data.old_username === currentUsername) {
+                currentUsername = data.new_username; 
+                currentUsernameSpan.textContent = `Your username: ${currentUsername}`
+            }
         })
+        
+        sendButton.addEventListener("click", sendMessage);
+        messageInput.addEventListener("keypress", (e) => {
+            if (e.key === "Enter") {
+                sendMessage(); 
+            }
+        })
+
+        updateUsernameButton.addEventListener("click", updateUsername)
+
+        function sendMessage() {
+            const message = messageInput.value.trim();
+            if(message) {
+                socket.emit("message", {message});
+                messageInput.value = ""; 
+            }
+        }
+
+        function updateUsername() {
+            const newUsername = usernameInput.value.trim(); 
+            if (newUsername && newUsername !== currentUsername) {
+                socket.emit("update_username", {username: newUsername}); 
+                usernameInput.value = ""; 
+            }
+        }
+        function addMessage() {}
